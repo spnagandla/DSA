f383b8387348330a2e4ae1871140d5beb1b17612
SaiPavanN <saipavannagandla007@gmail.com>
2026-02-14 15:23:53 -0600

    Frequency of array using the hashMap

--- src/Hashing/HighestOccurrence.java
+++ src/Hashing/HighestOccurrence.java
@@ -1,9 +1,12 @@
 package Hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HighestOccurrence {
 
     public static void findOccurrence(int[] arr){
-        int max =Integer.MAX_VALUE;
+        int max =0;
         for(int i=0;i<arr.length;i++){
             max = Math.max(max,arr[i]);
         }
@@ -25,10 +28,36 @@ public class HighestOccurrence {
         System.out.println(maxEle);
     }
 
+    public static void findOccurrence_HashMap(int[] arr){
+
+        Map<Integer,Integer> frequency = new HashMap<>();
+
+        for(int num : arr){
+            frequency.put(num,frequency.getOrDefault(num,0)+1);
+        }
+
+        int maxFreq =0;
+        int ans = Integer.MAX_VALUE;
+
+        for(Map.Entry<Integer,Integer> e : frequency.entrySet()){
+
+            int num = e.getKey();
+            int count = e.getValue();
+
+            if(count > maxFreq){
+                maxFreq = count;
+                ans = num;
+            } else if(count == maxFreq){
+                ans = Math.min(ans,num);
+            }
+        }
+        System.out.println(ans);
+    }
 
     public static void main(String[] args){
         int[] arr = {1, 2, 2, 3, 3, 3};
         findOccurrence(arr);
+        findOccurrence_HashMap(arr);
     }
 }
 
@@ -49,4 +78,28 @@ Example 2
 Input: nums = [4, 4, 5, 5, 6]
 Output: 4
 Explanation: Both 4 and 5 appear twice, but 4 is smaller. So, 4 is the most frequent element.
+
+
+not using hashMap:
+Time Complexity:
+
+O(n + m) where n is the length of nums and m is the maximum value in nums. The first loop iterates through nums once (O(n)). The second loop iterates through nums once (O(n)).
+The third loop iterates up to the maximum value (O(m)). Therefore, the total time complexity is O(n + m).
+Space Complexity
+O(m) where m is the maximum value in nums. An auxiliary array `visited` of size `max + 1` is created, where `max` is the maximum value in `nums`.
+This array stores the counts of each element up to the maximum value. Therefore, the space complexity is O(m).
+
+
+
+
+
+for (Map.Entry<Integer, Integer> e : freq.entrySet())
+
+freq.entrySet() → gives all key-value pairs
+Map.Entry<Integer, Integer> → type of each pair
+e → variable holding each pair
+: → means "for each"
+whole thing → "For each key-value pair in the map"
+
+freq = {1=1, 2=2, 3=3} => I1 e = (1,1) => I2 e = (2,2) => I3 e = (3,3)
  */
